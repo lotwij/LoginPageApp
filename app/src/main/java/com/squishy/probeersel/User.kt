3d7d1a1b4612a5af7b6e1fd9727628f3d62be88d
lotwij <Mormeltje4!>
2019-01-04 22:27:45 +0100

    Including first SQLdatabase. only need to fill in full name and age for now.

---
+++ app/src/main/java/com/squishy/probeersel/User.kt
@@ -0,0 +1,18 @@
+package com.squishy.probeersel
+
+class User{
+
+    var id: Int = 0
+    var name : String = ""
+    var age : Int = 0
+ ///   var email: String = ""
+
+    constructor(name:String, age:Int
+     ///           , email:String
+    ){
+        this.name = name
+        this.age = age
+///        this.email = email
+
+    }
+}
--- app/src/main/java/com/squishy/probeersel/UserRegistration.kt
+++ app/src/main/java/com/squishy/probeersel/UserRegistration.kt
@@ -2,11 +2,29 @@ package com.squishy.probeersel
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_user_registration.*
 
 class UserRegistration : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_registration)
+
+        val context = this
+
+        btn_insert.setOnClickListener {
+            if (etvName.text.toString().length>0 &&
+                    etvAge.text.toString().length > 0
+///                && etvEmail.text.toString().length > 0
+            ){
+                var user = User(etvName.text.toString(), etvAge.text.toString().toInt())
+     ///               ,etvEmail.text.toString())
+                var db = DataBaseHandler(context)
+                db.insertData(user)
+            }else{
+                Toast.makeText(context, "Please Fill in completely to register", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
